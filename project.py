f080245dd4fea42aa553e6bb8be552dcf58b446b
adriennette <92022862+adriennette@users.noreply.github.com>
2021-10-25 14:48:26 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from PIL import ImageTk, Image
+root = Tk()
+root.minsize(650,650)
+root.maxsize(650,650)
+root.configure(background="gray87")
+open_img = ImageTk.PhotoImage(Image.open ("open.png"))
+save_img = ImageTk.PhotoImage(Image.open ("save.png"))
+run_img = ImageTk.PhotoImage(Image.open ("run.png"))
+label_file_name = Label(root, text="File name")
+label_file_name.place(relx=0.28,rely=0.03,anchor= CENTER)
+
+input_file_name = Entry(root)
+input_file_name.place(relx=0.46,rely=0.03, anchor= CENTER)
+my_text= Text(root,height=35,width=80,bg = "slate gray", fg="white")
+my_text.place(relx=0.5,rely=0.55,anchor= CENTER)
+root.mainloop()
+"""
+Created on Mon Oct 25 14:43:02 2021
+
+@author: Admin
+"""
+
